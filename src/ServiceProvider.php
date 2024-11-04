a6798bc659b6ae55715d808f2c4d0b2ba4f681a5
Brian Faust <brian@faust.de.com>
2024-11-04 18:26:10 +0200

    first commit
    Signed-off-by: Brian Faust <brian@faust.de.com>

---
+++ src/ServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BaseCodeOy\BladeIcons\Lucide;
+
+use BaseCodeOy\BladeIcons\Facades\IconFamilyRegistry;
+use BaseCodeOy\BladeIcons\IconFamily;
+use BaseCodeOy\PackagePowerPack\Package\AbstractServiceProvider;
+
+final class ServiceProvider extends AbstractServiceProvider
+{
+    public function packageRegistered(): void
+    {
+        IconFamilyRegistry::push(IconFamily::fromDirectory('lucide', __DIR__.'/../resources/svg'));
+    }
+}
